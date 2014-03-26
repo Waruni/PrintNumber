3bc98de8a6b6c96e12813f76222aafbe7d018978
Waruni <wch@exilesoft.com>
2014-03-26 17:40:51 +0530

    refactoring

--- ConvertNumberToEnglishTest.cs
+++ ConvertNumberToEnglishTest.cs
@@ -11,78 +11,28 @@ namespace PrintNumber
 {
     public class ConvertNumberToEnglishTest
     {
-        readonly string[] _numbersUpTo19 = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-        readonly string[] _numbersFromTen = new[] { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        readonly string[] _numbersUpTo19 = new[] {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        readonly string[] _namesOfTens = new[] { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
 
-        [TestCase(1)]
-        public void Convert1ToOneTest(int number)
+        [TestCase(1, "one")]
+        [TestCase(2, "two")]
+        [TestCase(3, "three")]
+        [TestCase(20, "twenty")]
+        [TestCase(21, "twenty one")]
+        [TestCase(30, "thirty")]
+        [TestCase(35, "thirty five")]
+        [TestCase(100, "one hundred")]
+        [TestCase(110, "one hundred and ten")]
+        [TestCase(556, "five hundred and fifty six")]
+        [TestCase(7000, "seven thousand ")]
+        [TestCase(11812, "eleven thousand eight hundred and twelve")]
+        [TestCase(13014, "thirteen thousand  and fourteen")] 
+        public void ShouldConvertToEnglish(int number, string text)
         {
-            ToEnglish(number).Should().Be("one");
+            ToEnglish(number).Should().Be(text);
         }
-
-        [TestCase(2)]
-        public void Convert2ToTwoTest(int number)
-        {
-            ToEnglish(number).Should().Be("two");
-        }
-
-        [TestCase(3)]
-        public void Convert3ToThreeTest(int number)
-        {
-            ToEnglish(number).Should().Be("three");
-        }
-
-        [TestCase(20)]
-        public void Convert20ToTwentyTest(int number)
-        {
-            ToEnglish(number).Should().Be("twenty");
-        }
-
-        [TestCase(21)]
-        public void Convert21ToTwentyOneTest(int number)
-        {
-            ToEnglish(number).Should().Be("twenty one");
-        }
-         
-
-        [TestCase(30)]
-        public void Convert30ToThirtyTest(int number)
-        {
-            ToEnglish(number).Should().Be("thirty");
-        }
-
-        [TestCase(35)]
-        public void Convert35ToThirtyFiveTest(int number)
-        {
-            ToEnglish(number).Should().Be("thirty five");
-        }
-
-        [TestCase(100)]
-        public void Convert100ToOneHundredTest(int number)
-        {
-            ToEnglish(number).Should().Be("one hundred");
-        }
-
-        [TestCase(110)]
-        public void Convert110ToOneHundredAndTenTest(int number)
-        {
-            ToEnglish(number).Should().Be("one hundred and ten");
-        }
-
-
-        [TestCase(556)]
-        public void Convert556ToFiveHundredAndFiftySixTest(int number)
-        {
-            ToEnglish(number).Should().Be("five hundred and fifty six");
-        }
-
-        [TestCase(7000)]
-        public void Convert7000ToSevenThousandTest(int number)
-        {
-            ToEnglish(number).Should().Be("seven thousand ");
-        }
-
+        
         [TestCase(11812)]
         public void Convert11812ToElevenThousandEightHundredAndTwelveTest(int number)
         {
@@ -97,18 +47,24 @@ namespace PrintNumber
 
         private string ToEnglish(int number)
         {
-            string text = "";  
+            string text = "";
+
+            if ((number / 1000000) > 0)
+            {
+                text += ToEnglish(number / 1000000) + " million";
+                number %= 1000000;
+            }
 
             if ((number / 1000) > 0)
             {
                 text += ToEnglish(number / 1000) + " thousand ";
-                number = number % 1000;
+                number %= 1000;
             }
 
             if ((number / 100) > 0)
             {
                 text += ToEnglish(number / 100) + " hundred";
-                number  = number % 100;
+                number  %= 100;
             }
             
             if (number > 0)
@@ -127,16 +83,16 @@ namespace PrintNumber
         {
             if (number < 20)
             {
-                text += _numbersUpTo19[number - 1];
+                text += _numbersUpTo19[number];
             }
             else
             {
-                text += _numbersFromTen[number/10];
+                text += _namesOfTens[number/10];
 
                 int remain = number%10;
 
                 if (remain > 0)
-                    text += " " + _numbersUpTo19[remain - 1];
+                    text += " " + _numbersUpTo19[remain];
             }
             return text;
         }
